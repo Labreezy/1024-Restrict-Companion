112bb209e4e9358b550b4bd05262830e9f35a621
labreezy <yourtooslow@gmail.com>
2023-02-22 10:41:27 -0600

    add p1/p2 confirmed data feature + empty sets.txt functionality

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import sys
 import frida
+import json
 import os.path as osp
+confirmed_display = -1
 def on_msg(message, data):
-    print(message)
+
+    if message["type"] == "send":
+        if len(message['payload']) == 2 and confirmed_display == 1:
+            set_id = message["payload"][1]
+            if set_id >= 0:
+                stage_id = str(message['payload'][0])
+                stage_sets = SET_DATA[stage_id]
+                p1, p2, p3 = stage_sets[set_id]
+                sets_with_combo = [s for s in stage_sets if s[0]==p1 and s[1]==p2]
+                if len(sets_with_combo) == 1:
+                    print("Set is confirmed")
+                else:
+                    print(f"P1/2 does not confirm P3, {len(sets_with_combo)} Possibilities:")
+                    sorted_with_combo = sorted(sets_with_combo,key=lambda s: s[-1])
+                    for hints in sorted_with_combo:
+                        print("//".join(hints))
+                print("-----")
+
+    else:
+        print(message)
+
+
 
 if __name__ == '__main__':
     session = frida.attach("sonic2app.exe");
@@ -18,6 +41,12 @@ if __name__ == '__main__':
     print("1 - With replacement [default]")
     print("0 - Without replacement")
     replacement = int(input("Choice:") or 1)
+    print("Choose to display live confirmed set data")
+    print("1 - Yes")
+    print("2 - No")
+    confirmed_display = int(input("Choice:") or 2)
+
+    SET_DATA = json.load(open('1024_data.json'))
     print("Ready to go!")
     msgdat = int(safecolor) - 1
     script.post({"type": "safe","id": msgdat})
@@ -28,5 +57,6 @@ if __name__ == '__main__':
         script.post({"type": "sets","sets": ids})
     else:
         script.post({"type": "sets","sets": []})
+
     sys.stdin.read()
 
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var orderbase = Module.findBaseAddress('order.dll');
 const HUNT_STAGE_IDS = [5,7,8,16,18,25,26,32,44];
 const LDATA_STRUCT_SIZE = 0xC;
 var SETS_TO_PRACTICE = [];
+var curr_stage_ptr = ptr(0x1934b70);
 //rng shuffling
 var deathsrestartsptr = base.add(0x1534be8);
 var setsafesptr = base.add(0x16edfd4);
@@ -12,6 +13,7 @@ var nextseedptr = orderbase.add(0x117810);
 var fnsetstate = base.add(0x3380a0);
 //security hall
 var fninitsecurityhall = base.add(0x235440);
+var fnemeraldmanagerupdate = base.add(0x3395B8);
 var safe = -1;
 var safeset = false;
 //infinite lives
@@ -21,6 +23,7 @@ var tscopeaddr = base.add(0x19eb31a);
 //Replacement/no replacement handling
 var replacement = true;
 var sets_to_sample = [];
+var current_setid = -1;
 //exit game safety
 /*var entermainmenuptr = base.add(0x284486);
 var levelcount = -1;
@@ -38,8 +41,25 @@ while(thisid != 2){
 var internalleveldata = leveldataptr.readByteArray((n_levels+1)*LDATA_STRUCT_SIZE);
 var internalleveldataptr = leveldataptr;
 */
-recv("safe", function(msg){
+var m_w = 123456789;
+var m_z = 987654321;
+var mask = 4294967295; //0xFFFFFFFF
+//since js can't seed its rng i said fuck it and made my own
+function seed(i){
+    m_w = (123456789 + i) & mask;
+    m_z = (987654321 - i) & mask;
+}
+function seedable_random_u16(){
+    m_z = (36969 * (m_z & 65535) + (m_z >> 16)) & mask;
+    m_w = (18000 * (m_w & 65535) + (m_w >> 16)) & mask;
+    var result = ((m_z << 16) + (m_w & 65535)) >>> 0;
+    result /= 4294967296; //result is anywhere between 0.0-1.0
+    result *= 65535; //gets it into uint16 range
+    result = Math.floor(result);
+    return result;
+}
 
+recv("safe", function(msg){
     safe = msg.id;
 });
 
@@ -56,29 +76,35 @@ recv("sets", function(msg){
 Interceptor.attach(fnsetstate, {
    onEnter(args){
        var drs = deathsrestartsptr.readU8();
-
        if(drs > 0){
-           var cseed = nextseedptr.readU16();
-           cseed += 0x1327;
-           cseed = cseed % 65535;
-           nextseedptr.writeU16(cseed); 
-           console.log("Wrote new state");
+           var curr_rand = seedable_random_u16();
+           nextseedptr.writeU16(curr_rand);
+           console.log("Wrote new state " + curr_rand);
        } else {
            if(SETS_TO_PRACTICE.length > 0){
                if (replacement) {
                     var rand_idx = Math.floor(Math.random() * SETS_TO_PRACTICE.length);
                     nextseedptr.writeU16(SETS_TO_PRACTICE[rand_idx]);
+                    current_setid = SETS_TO_PRACTICE[rand_idx]
                     console.log("Wrote " + SETS_TO_PRACTICE[rand_idx]);
                } else {
                     var rand_idx = Math.floor(Math.random() * sets_to_sample.length);
                     nextseedptr.writeU16(sets_to_sample[rand_idx]);
+                    current_setid = sets_to_sample[rand_idx];
                     console.log("Wrote " + sets_to_sample[rand_idx]);
                     sets_to_sample.splice(rand_idx, 1);
                     if (sets_to_sample.length == 0) {
                         sets_to_sample = [...SETS_TO_PRACTICE];
                     }
                }
+
+           } else {
+                var rand_idx = Math.floor(Math.random()*1024);
+                nextseedptr.writeU16(rand_idx);
+               // console.log("Wrote random set " + rand_idx);
+                current_setid = rand_idx;
            }
+           seed(nextseedptr.readU16()); //seed for deaths/restarts later
        }
        //lives
        var lives = lifeptr.readU16();
@@ -92,6 +118,24 @@ Interceptor.attach(fnsetstate, {
        }
    }
 });
+var last_state = 0;
+var last_emeralds_spawned = 3;
+//is it confirmed stuff
+Interceptor.attach(fnemeraldmanagerupdate, { onEnter(){
+    var manager = this.context.edi;
+    var state = manager.readU8();
+    var emeralds_spawned_ptr = manager.add(5)
+    var emeralds_spawned = emeralds_spawned_ptr.readU8();
+    if(state == 4 && emeralds_spawned != last_emeralds_spawned){
+        console.log("Piece collected (" + emeralds_spawned + " piece(s) remaining)");
+        if(emeralds_spawned == 0){
+            send([curr_stage_ptr.readU8(),current_setid]);
+        }
+    }
+    last_state = state;
+    last_emeralds_spawned = emeralds_spawned;
+}});
+
 //safes
 Interceptor.attach(fninitsecurityhall, {
     onEnter(args){
